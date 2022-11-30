937da29f985d6ae9e3fa91909b23cd76f3adb904
gbeloso <gabrielbeloso62@gmail.com>
2022-11-30 20:30:28 -0300

    so pra enviar

---
+++ test/entities/todoitemtest.py
@@ -0,0 +1,18 @@
+from src.entities.todoitem import TodoItem
+from src.entities.user import User
+
+def test_done():
+    item = TodoItem('make bed', Priority.LOW)
+    item.complete()
+    assert item.is_completed() == True
+
+def test_undone():
+    item = TodoItem('make bed', Priority.LOW)
+    item.complete()
+    item.undo()
+    assert item.is_completed() == False
+
+def test_change_description():
+    item = TodoItem('make bed', Priority.LOW)
+    item.change_description('change bed')
+    assert item.description == 'change bed'
